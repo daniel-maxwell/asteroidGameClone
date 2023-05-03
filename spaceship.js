32f3047c3b353d8f7151c517dc4e6f229454de7f
Daniel White <dm.white@gmx.com>
2023-05-03 02:35:08 +0100

    Starting code

---
+++ spaceship.js
@@ -0,0 +1,59 @@
+class Spaceship {
+
+  constructor(){
+    this.velocity = new createVector(0, 0);
+    this.location = new createVector(width/2, height/2);
+    this.acceleration = new createVector(0, 0);
+    this.maxVelocity = 5;
+    this.bulletSys = new BulletSystem();
+    this.size = 50;
+  }
+
+  run(){
+    this.bulletSys.run();
+    this.draw();
+    this.move();
+    this.edges();
+    this.interaction();
+  }
+
+  draw(){
+    fill(125);
+    triangle(this.location.x - this.size/2, this.location.y + this.size/2,
+        this.location.x + this.size/2, this.location.y + this.size/2,
+        this.location.x, this.location.y - this.size/2);
+  }
+
+  move(){
+  }
+
+  applyForce(f){
+    this.acceleration.add(f);
+  }
+
+  interaction(){
+      if (keyIsDown(LEFT_ARROW)){
+        this.applyForce(createVector(-0.1, 0));
+      }
+      if (keyIsDown(RIGHT_ARROW)){
+      }
+      if (keyIsDown(UP_ARROW)){
+      }
+      if (keyIsDown(DOWN_ARROW)){
+      }
+  }
+
+  fire(){
+    this.bulletSys.fire(this.location.x, this.location.y);
+  }
+
+  edges(){
+    if (this.location.x<0) this.location.x=width;
+    else if (this.location.x>width) this.location.x = 0;
+    else if (this.location.y<0) this.location.y = height;
+    else if (this.location.y>height) this.location.y = 0;
+  }
+
+  setNearEarth(){
+  }
+}
